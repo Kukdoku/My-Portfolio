44f2485ea74386e822097c63836fcd5b449489b2
Kukdoku <visheshmzn99@gmail.com>
2021-08-15 10:41:23 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import myprofile from "./images/myprofile.jpg";
+
 
 function App() {
+  const scrollProperty = () => {
+    if (window.scrollY > 20) {
+      return "sticky";
+    } else {
+      return "";
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        {/* navbar Header */}
+        <div className={`navbar ${scrollProperty()}`}>
+          <div className="max-width">
+            <div className="logo">
+              <Link to="#">
+                Portfo<span>lio.</span>
+              </Link>
+            </div>
+            <ul className="menu">
+              <li>
+                <Link to="#">Home</Link>
+              </li>
+              <li>
+                <Link to="#">About</Link>
+              </li>
+              <li>
+                <Link to="#">Services</Link>
+              </li>
+              <li>
+                <Link to="#">Skills</Link>
+              </li>
+              <li>
+                <Link to="#">Projects</Link>
+              </li>
+              <li>
+                <Link to="#">Resume</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        {/* Home Section Start Here  */}
+        <div className="home" id="home">
+          <div>
+            <img src={myprofile} alt="profilePic" className="profilePic" />
+          </div>
+          <div className="max-width">
+            <div className="home-content">
+              <div className="text-1">Hello My Name is </div>
+              <div className="text-2">Vishesh Kumar </div>
+              <div className="text-3">
+                I am <span>Web Developer</span>{" "}
+              </div>
+            </div>
+          </div>
+        </div>
+        <p>lorem30dsfasdjfakdfa;fk</p>
+        <p>lorem30dsfasdjfakdfa;fk</p>
+        <p>lorem30dsfasdjfakdfa;fk</p>
+        <p>lorem30dsfasdjfakdfa;fk</p>
+        <p>lorem30dsfasdjfakdfa;fk</p>
+        <p>lorem30dsfasdjfakdfa;fk</p>
+      </div>
+    </Router>
   );
 }
 
